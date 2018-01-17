08c3c1c00677a2e7a210a783da751f6cc25ad391
Nathaniel Hoag <info@nathanielhoag.com>
2018-01-16 21:21:22 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,43 +1,63 @@
 #!/usr/bin/env bash
 
+# Bail on any error.
+set -e
+set -o pipefail
+
+# Convenience function for calling special dotfiles git.
 function dots() {
-  git --git-dir="$HOME/$DOTS_DIR" --work-tree="$HOME" "$@"
+  git \
+    --git-dir="$HOME/$DOTS_DIR" \
+    --work-tree="$HOME" \
+    "$@"
 }
 
 function install() {
 
-  local DOTS_DIR=${1:-'.dotfiles'}
-
-  [[ -d "$HOME/$DOTS_DIR" ]] && { echo 'Already installed.'; return; }
-
-  requirements
-
   cd
 
+  # Clone to a throw-away location.
   git clone \
     --separate-git-dir="$HOME/$DOTS_DIR" \
     git@github.com:nhoag/dotfiles.git \
     $(mktemp -d)
 
   dots config status.showUntrackedFiles no
-
   dots checkout .
 
+  # Switch the default shell to zsh (requires sudo).
   chsh -s $(which zsh)
 
+  # Use zsh right now (and trigger zgen build).
   zsh
 
   cd -
 
 }
 
-function requirements() {
+# Main installer function.
+function main() {
 
-  for cmd in curl git vim zsh; do
-    command -v $cmd >/dev/null 2>&1 || { echo >&2 "I require $cmd but it's not installed. Aborting."; exit 1; }
-  done
+  local DOTS_DIR=${1:-'.dotfiles'}
+
+  [[ -d "$HOME/$DOTS_DIR" ]] && { echo 'Already installed.'; return; }
+
+  requirements \
+    curl \
+    git \
+    vim \
+    zsh
+
+  install
 
 }
 
-install "$@"
+# Check requirements are satisfied.
+function requirements() {
+  for cmd in "$@"; do
+    command -v $cmd >/dev/null 2>&1 || { echo >&2 "I require $cmd but it's not installed. Aborting."; exit 1; }
+  done
+}
+
+main "$@"
 
