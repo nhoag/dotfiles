eb756f3386f4c4abc721fca0e1a9063c770b7197
Nathaniel Hoag <info@nathanielhoag.com>
2019-04-26 16:55:34 -0400

    Add emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+;; Package config
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(require 'org)
+(org-babel-load-file "/Users/nathaniel.hoag/.emacs.d/emacs.org")
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
